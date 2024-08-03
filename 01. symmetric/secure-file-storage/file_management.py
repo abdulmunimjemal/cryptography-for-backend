b7382e18de55fba57ae476e80f72d1b568b60bc7
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-08-03 21:46:59 +0300

    [ENH] Added File Management Module

---
+++ 01. symmetric/secure-file-storage/file_management.py
@@ -0,0 +1,18 @@
+import os
+
+class FileManagementModule:
+    def __init__(self, storage_dir):
+        self.storage_dir = storage_dir
+        if not os.path.exists(storage_dir):
+            os.makedirs(storage_dir)
+    
+    def save_file(self, file_name, data):
+        with open(os.path.join(self.storage_dir, file_name), 'wb') as f:
+            f.write(data)
+        
+    def read_file(self, file_name):
+        with open(os.path.join(self.storage_dir, file_name), 'wb') as f:
+            return f.read()
+    
+    def list_files(self):
+        return os.listdir(self.storage_dir)
