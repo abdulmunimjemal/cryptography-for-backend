6aaad43a424c744159b9084a66ae1bfd2bad7142
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-08-03 21:31:03 +0300

    [ENH] Added Key Derivation Algorithm

--- 01. symmetric/secure-file-storage/encryption.py
+++ 01. symmetric/secure-file-storage/encryption.py
@@ -1,7 +1,11 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
 import os
 
+
+# Using Strategy Pattern
 class EncryptionStrategy:
     def encrypt(self, key, data):
         raise NotImplementedError
@@ -22,3 +26,39 @@ class AESEncryption(EncryptionStrategy):
         decryptor = cipher.decryptor()
         decrypted_data = decryptor.update(data[16:]) + decryptor.finalize()
         return decrypted_data
+
+class EncryptionContext:
+    def __init__(self, strategy: EncryptionStrategy):
+        self.strategy = strategy
+    
+    def encrypt(self, key, data):
+        return self.strategy.encrypt(key, data)
+    
+    def decrypt(self, key, data):
+        return self.strategy.decrypt(key, data)
+    
+# Generate a key from a password
+class KeyDerivation:
+    def __init__(self, password):
+        self.password = password.encode()
+        self.salt = os.urandom(16)
+    
+    def derive_key(self):
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=self.salt,
+            iterations=100000,
+            backend=default_backend()
+        )
+        return kdf.derive(self.password)
+
+# For those wondering, example usage
+
+# ----- get key from pass --------
+# key_derivation = KeyDerivation("my_password")
+# key = key_derivation.derive_key()
+# ----- Encryption / Decryption --------
+# encryption_context = EncryptionContext(AESEncryption())
+# encrypted_data = encryption_context.encrypt(key, b"My secret data")
+# decrypted_data = encryption_context.decrypt(key, encrypted_data)
